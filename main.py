28b14749591e518cd8a05ba683d0e828e1e9d554
Will sumfest <was022@berkeley.edu>
2017-03-20 11:35:01 -0700

    added main.py hello world script

---
+++ main.py
@@ -0,0 +1,6 @@
+import sys
+
+sys.stdout.write("Hello World!")
+sys.stdout.write(" ")
+sys.stdout.write("I am excited to be considered for the Application Developer role with the Clinical and Translational Sciences Institute at UF!")
+sys.stdout.write("\n")
